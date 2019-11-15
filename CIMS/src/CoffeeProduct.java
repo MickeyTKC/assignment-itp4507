14d1a45fa719b5e4bfb2a1811e46bdc873b63026
MickeyTKC <tt51145505@gmail.com>
2019-11-16 00:29:40 +0800

    +clone() to Products

--- CIMS/src/CoffeeProduct.java
+++ CIMS/src/CoffeeProduct.java
@@ -29,6 +29,11 @@ public class CoffeeProduct {
     public void setQty(int qty) {
         this.qty = qty;
     }
+    public CoffeeProduct clone(){
+        CoffeeProduct temp = new CoffeeProduct(this.name,this.productID);
+        temp.setQty(this.qty);
+        return temp;
+    }
 }
 
 class Candy extends CoffeeProduct {
@@ -53,6 +58,12 @@ class Candy extends CoffeeProduct {
     public void setCaloriesPerCandy(int caloriesPerCandy) {
         this.caloriesPerCandy = caloriesPerCandy;
     }
+    @Override
+    public CoffeeProduct clone(){
+        CoffeeProduct temp = new Candy(this.getProductID(),this.getName(),this.getNoOfCandy(),this.getCaloriesPerCandy());
+        temp.setQty(this.getQty());
+        return temp;
+    }
 }
 
 class Powder extends CoffeeProduct{
@@ -68,5 +79,11 @@ class Powder extends CoffeeProduct{
     }
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+    @Override
+    public CoffeeProduct clone(){
+        CoffeeProduct temp = new Powder(this.getProductID(),this.getName(),this.getWeight());
+        temp.setQty(this.getQty());
+        return temp;
     }
 }
