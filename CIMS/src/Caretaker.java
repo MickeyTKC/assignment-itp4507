d27ea1105fbcc5d7ba1bd57fd3501927f55c7c16
MickeyTKC <tt51145505@gmail.com>
2019-12-04 17:56:18 +0800

    +Caretaker
    handler of Memento

---
+++ CIMS/src/Caretaker.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+public class Caretaker {
+
+    private Stack<Memento> undoList;
+    private Stack<Memento> redoList;
+
+    public Caretaker() {
+        undoList = new Stack<Memento>();
+        redoList = new Stack<Memento>();
+    }
+
+    public void saveMyClass(CoffeeShop sc) {
+        Memento amemento = new Memento(sc);
+        if (!redoList.empty()) {
+            if (!amemento.equals(redoList.get(redoList.size() - 1))) {
+                redoList.clear();
+            }
+        }
+        undoList.push(amemento);
+    }
+
+     public boolean undo() {
+        if (undoList.size()>1) {
+            Memento temp = undoList.pop();
+            redoList.push(temp);
+             Memento m  = undoList.get(undoList.size()-1);
+            m.restore();
+            System.out.println("Perform undo");
+            return true;
+        } else {
+            System.out.println("Nothing to undo.");
+            return false;
+        }
+    }
+
+    public boolean redo() {
+        if (!redoList.isEmpty()) {
+            Memento m = redoList.pop();
+            undoList.push(m);
+            m.restore();
+            System.out.println("Perform redo");
+            return true;
+        } else {
+            System.out.println("Nothing to redo.");
+            return false;
+        }
+    }
+}
