8ec9015fef9f110de87d4a1961c8bf2caf93795a
MickeyTKC <tt51145505@gmail.com>
2019-12-04 21:49:16 +0800

    Complete Caretaker
    +void:save()
    +void:undo()
    +void:redo()
    +String:records()

--- CIMS/src/Caretaker.java
+++ CIMS/src/Caretaker.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class Caretaker {
 
-    private Stack<Memento> undoList;
-    private Stack<Memento> redoList;
+    public Stack<Memento> undoList;
+    public Stack<Memento> redoList;
 
     public Caretaker() {
         undoList = new Stack<Memento>();
         redoList = new Stack<Memento>();
     }
-    
-    public void save(CoffeeShop sc,String action) {
-        Memento amemento = new Memento(sc,action);
+
+    public void save(CoffeeShop cs, String action) {
+        Memento amemento = new Memento(cs, action);
         if (!redoList.empty()) {
             if (!amemento.equals(redoList.get(redoList.size() - 1))) {
                 redoList.clear();
@@ -21,16 +21,14 @@ public class Caretaker {
         undoList.push(amemento);
     }
 
-     public boolean undo() {
-        if (undoList.size()>1) {
+    public boolean undo() {
+        if (undoList.size() > 1) {
             Memento temp = undoList.pop();
             redoList.push(temp);
-             Memento m  = undoList.get(undoList.size()-1);
+            Memento m = undoList.get(undoList.size() - 1);
             m.restore();
-            System.out.println("Perform undo");
             return true;
         } else {
-            System.out.println("Nothing to undo.");
             return false;
         }
     }
@@ -40,15 +38,26 @@ public class Caretaker {
             Memento m = redoList.pop();
             undoList.push(m);
             m.restore();
-            System.out.println("Perform redo");
             return true;
         } else {
-            System.out.println("Nothing to redo.");
             return false;
         }
     }
-    
-    public String records(){
-        return "";
+
+    public String records() {
+        String s = "";
+        if (!undoList.empty()) {
+            s+="Undo List:\n";
+            for (int i = 1; i < undoList.size(); i++) {
+                s+=undoList.get(i).getAction()+"\n";
+            }
+        }
+        if (!redoList.empty()) {
+            s+="Redo List:\n";
+            for (int i = 0; i < redoList.size(); i++) {
+                s+=redoList.get(i).getAction()+"\n";
+            }
+        }
+        return s;
     }
 }
