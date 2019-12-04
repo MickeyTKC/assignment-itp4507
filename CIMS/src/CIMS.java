09e198c64b851b8bb314b8c325cb219a226fbbb1
MickeyTKC <tt51145505@gmail.com>
2019-12-04 22:44:42 +0800

    rework CIMS
    for the CoffeeCommandFactory

--- CIMS/src/CIMS.java
+++ CIMS/src/CIMS.java
@@ -7,23 +7,22 @@ public class CIMS {
         Scanner sc = new Scanner(System.in);
         CoffeeShop shop = new CoffeeShop("Coffee Shop");
         Caretaker record = new Caretaker();
-        final CoffeeCommand[] coffeeCommand = {
-            new CommandAddProduct("a", sc, shop,record),
-            new CommandCollectProduct("c", sc, shop,record),
-            new CommandShipProduct("s", sc, shop,record),
-            new CommandViewProduct("v", sc, shop),
-            new CommandUndo("u", sc, record),
-            new CommandRedo("r", sc, record),
-            new CommandURList("sl",sc,record),
-            new CommandExit("x", sc),
-        };
-        record.save(shop,"Create Coffee Shop");
+        final CoffeeCommandFactory[] coffeeCommand = {
+            new AddProductFactory("a", sc, shop, record),
+            new CollectProductFactory("c", sc, shop, record),
+            new ShipProductFactory("s", sc, shop, record),
+            new ViewProductFactory("v", sc, shop),
+            new UndoFactory("u", sc, record),
+            new RedoFactory("r", sc, record),
+            new URListFactory("sl", sc, record),
+            new ExitFactory("x", sc),};
+        record.save(shop, "Create Coffee Shop");
         while (true) {
             System.out.println("Please enter command: [a | v | c | s | u | r | sl | x]");
             String input = sc.next();
-            for (CoffeeCommand cmd : coffeeCommand) {
-                if (cmd.name().equals(input)) {
-                    cmd.execute();
+            for (CoffeeCommandFactory cf : coffeeCommand) {
+                if (cf.name().equals(input)) {
+                    cf.create().execute();
                     break;
                 }
             }
